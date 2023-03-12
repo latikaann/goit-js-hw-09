626b1fe7d0f984ddc1057d9d01e6ae4f93d8fad7
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-03-12 23:41:29 +0200

    save

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
+// console.log(refs);
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStopButtonClick);
+
+let timerId = null;
+const DELAY = 1000;
+isActive = true;
+
+function onStartButtonClick(e) {
+  console.log(e.target);
+  if (this.isActive) {
+    return;
+  }
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  this.isActive = true;
+}
+
+// setInterval(onStartButtonClick, DELAY);
+function onStopButtonClick(e) {
+  clearInterval(timerId);
+}
